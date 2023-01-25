010adcda391510f28368524e75ac33d015aa913d
Danil-891 <120020208+Danil-891@users.noreply.github.com>
2023-01-25 14:26:07 +0300

    Add files via upload

---
+++ dkr-7.pas
@@ -0,0 +1,22 @@
+﻿uses graphabc;
+procedure levy(x,y,a,r:real;k:integer); 
+begin
+if k>0 then 
+ begin
+  r:=r*sin(pi/4);
+  levy(x,y,a+pi/4,r,k-1);
+  levy(x+r*cos(a+pi/4),y-r*sin(a+pi/4),a-pi/4,r,k-1);
+ end 
+else
+line(trunc(x),trunc(y),trunc(x+r*cos(a)),trunc(y-r*sin(a))); 
+end; 
+begin
+  var c,d,e:integer;
+  writeln('Введите глубину прорисовки');
+  read(c);
+  writeln('Введите масштаб кривой');
+  read(d);
+  writeln('Введите куда переместить кривую');
+  read(e);
+levy(e,300,0,d,c); 
+end.
